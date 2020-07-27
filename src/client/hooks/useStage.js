96093df94028941cdc7938bb792b00f08a753329
veronar <vishakha108@gmail.com>
2020-07-28 01:27:26 +0200

    clearing of complete rows done

--- src/client/hooks/useStage.js
+++ src/client/hooks/useStage.js
@@ -4,8 +4,22 @@ import { createStage } from '../helpers/gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
 	const [stage, setStage] = useState(createStage());
+	const [rowsCleared, setRowsCleared] = useState(0);
 
 	useEffect(() => {
+		setRowsCleared(0);
+
+		const sweepRows = (newStage) =>
+			newStage.reduce((ack, row) => {
+				if (row.findIndex((cell) => cell[0] === 0) === -1) {
+					setRowsCleared((prev) => prev + 1);
+					ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+					return ack;
+				}
+				ack.push(row);
+				return ack;
+			}, []);
+
 		const updateStage = (prevStage) => {
 			// first flush stage from the previous render
 			const newStage = prevStage.map((row) =>
@@ -27,6 +41,7 @@ export const useStage = (player, resetPlayer) => {
 			// then check if collided
 			if (player.collided) {
 				resetPlayer();
+				return sweepRows(newStage);
 			}
 
 			return newStage;
@@ -35,5 +50,5 @@ export const useStage = (player, resetPlayer) => {
 		setStage((prev) => updateStage(prev));
 	}, [player, resetPlayer]);
 
-	return [stage, setStage];
+	return [stage, setStage, rowsCleared];
 };
