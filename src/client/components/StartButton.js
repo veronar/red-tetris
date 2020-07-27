25b1344f252fb29b9ce41baa368f4f356e820bb7
veronar <vishakha108@gmail.com>
2020-07-27 22:26:52 +0200

    all styles, components and shapes done - next to do game logic

--- src/client/components/StartButton.js
+++ src/client/components/StartButton.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import { StyledStartButton } from './styles/StyledStartButton';
+
 const StartButton = ({ callback }) => (
-	<div>Start Game</div>
+	<StyledStartButton onclick={callback}>
+		Start Game
+	</StyledStartButton>
 );
 
 export default StartButton;
