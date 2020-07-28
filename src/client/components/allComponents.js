9273cd54f077d3f579dfce7025ff33245b66b764
veronar <vishakha108@gmail.com>
2020-07-28 11:59:26 +0200

    added componenets to testng

--- src/client/components/allComponents.js
+++ src/client/components/allComponents.js
@@ -1,9 +1,18 @@
 import Test from './test';
+
+// Components
 import Cell from './Cell';
 import Display from './Display';
 import Stage from './Stage';
 import StartButton from './StartButton';
 import Tetris from './Tetris';
+
+// Component Styles
+import StyledCell from './styles/StyledCell';
+import StyledDisplay from './styles/StyledDisplay';
+import StyledStage from './styles/StyledStage';
+import StyledStartButton from './styles/StyledStartButton';
+import StyledTetris from './styles/StyledTetris';
 
 export {
 	Test,
@@ -11,5 +20,10 @@ export {
 	Display,
 	Stage,
 	StartButton,
-	Tetris
-}
+	Tetris,
+	StyledCell,
+	StyledDisplay,
+	StyledStage,
+	StyledStartButton,
+	StyledTetris,
+};
