51f83ccb3d0ed0f8a6e1574b32487928f2b689fe
veronar <vishakha108@gmail.com>
2020-07-30 15:37:50 +0200

    fixed start button prop issue

--- src/client/components/StartButton.js
+++ src/client/components/StartButton.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Button, Typography } from '@material-ui/core';
 
-import { StyledStartButton } from './styles/StyledStartButton';
+// import { StyledStartButton } from './styles/StyledStartButton';
 
 const StartButton = ({ callback }) => (
 	<Button
 		variant="contained"
-		color="warning"
 		id="startButton"
 		onClick={callback}
-		fullWidth="true"
+		fullWidth
 	>
 		{/* <StyledStartButton id="startButton" onClick={callback}> */}
 		<Typography>START GAME</Typography>
-		{/* </StyledStartButton> */}
+
 	</Button>
 );
 
