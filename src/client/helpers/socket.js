b0822cc4c11ce10cb3b0d9dc56e7ce1c6ce71a5a
fwmoor <fwmoor@outlook.com>
2020-08-09 16:47:09 +0200

    Rooms are working. Not 100% yet, but working

--- src/client/helpers/socket.js
+++ src/client/helpers/socket.js
@@ -6,6 +6,7 @@ const userSocket = (room) => {
 		if (mainSocket?.connected === false || !mainSocket) {
 			mainSocket = io.connect("http://localhost:2000")
 			mainSocket.on("connection", () => {
+				mainSocket.emit('join', room)
 				resolve(mainSocket);
 			})
 		}
--- src/server/helpers/socket.js
+++ src/server/helpers/socket.js
@@ -1,13 +1,21 @@
 exports.makeSocket = io => {
 	io.on('connection', function (socket) {
 		console.log("Socket connected: " + socket.id)
+		socket.emit('connection')
 		socket.on('action', (action) => {
 			if (action.type === 'server/ping') {
 				socket.emit('action', { type: 'pong' })
 			}
 		})
+		socket.on('join', (room) => {
+			socket.join(room)
+			// Below give amount of users in current room
+			// io.of('/').in(room).clients(function (error, clients) {
+			// 	console.log(clients.length);
+			// });
+		})
 		socket.on('test', () => {
-			socket.broadcast.emit('idk')
+			socket.to(Object.keys(socket.rooms)[0]).emit('idk')
 		})
 	})
 }
